e979e821e1cdefe02fe10635e94b02120070ed78
Andrés Martínez <autummata@gmail.com>
2023-10-24 10:04:17 -0700

    first commit'

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text> This is very very basado </Text>
+      <Text> This is very epic </Text>
       <StatusBar style="auto" />
     </View>
   );
