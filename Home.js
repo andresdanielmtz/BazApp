0a0df49596a98f1f226a431e61d81015f5a9ff1c
Andrés Martínez <autummata@gmail.com>
2023-10-24 13:09:44 -0700

    redesigned home page, added logo

--- Home.js
+++ Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View, Image, Button, TextInput} from "react-native";
-
+import Logo from "./assets/logo.png"
 function Home() {
     const [valueLogin, onChangeTextLogin] = React.useState('Insert Login');
     const [valuePassword, onChangeTextPassword] = React.useState('Insert Password')
@@ -14,20 +14,22 @@ function Home() {
         <View style={styles.header}>
           <Image
             style={styles.logo}
-            source={{ uri: "https://your-logo-url.com/logo.png" }}
+            source={Logo}
           />
           <Text style={styles.title}>BazApp</Text>
-          <Text style={styles.subtitle}>Your One-Stop Bazaar Hub</Text>
+          <Text style={styles.subtitle}>Tu Bazaar a un dedo de distancia.</Text>
         </View>
-  
+        
+
         <View style={styles.content}>
           <Text style={styles.description}>
-            Discover endless bazaar items from multiple vendors all in one place.
+            Descubre multiples items de bazaar de varios vendedores todo en un solo lugar.
   
           </Text>
   
-          <Text> Login </Text> 
-          <Text> Username </Text>
+
+          <Text style = {styles.title_main}> Login </Text> 
+          <Text style = {styles.title_login}> Username </Text>
           <TextInput
           editable
           multiline
@@ -35,10 +37,15 @@ function Home() {
           maxLength={40}
           onChangeText={text => onChangeTextLogin(text)}
           value={valueLogin}
-          style={{padding: 10}}
+          style={styles.textView}
+          placeholder="Insert Account Username"
+          placeholderTextColor="#202020"
+          returnKeyType='go'
+          secureTextEntry
+          autoCorrect={false}
         />
   
-        <Text> Password </Text> 
+        <Text style = {styles.title_login}> Password </Text> 
         <TextInput
           editable
           multiline
@@ -46,8 +53,14 @@ function Home() {
           maxLength={40}
           onChangeText={text => onChangeTextPassword(text)}
           value={valuePassword}
-          style={{padding: 10}}
+          style={styles.textView}
+          placeholder="Insert Account Password"
+          placeholderTextColor="#202020"
+          returnKeyType='go'
+          secureTextEntry
+          autoCorrect={false}
         />
+        </View> 
         <Button
         title="Vista del Cliente"
         onPress={() => navigation.navigate('Buyer')}
@@ -57,12 +70,9 @@ function Home() {
       title = "Vista del Vendedor"
       onPress = {() => navigation.navigate('Seller')}
       /> 
+      
         </View>
-
-
-
   
-      </View>
     );
   }
   
@@ -74,7 +84,7 @@ function Home() {
     header: {
       paddingTop: 60,
       paddingBottom: 20,
-      backgroundColor: "#ba7f36",
+      backgroundColor: "#00192A",
       alignItems: "center",
       justifyContent: "center",
     },
@@ -104,6 +114,43 @@ function Home() {
       textAlign: "center",
       marginBottom: 20,
     },
-  });
+    textView: { 
+      borderLeftWidth: 1,
+      borderRightWidth: 1,
+      borderTopWidth: 1, 
+      borderBottomWidth: 1,
+      borderRadius: 10,
+      padding: 10, 
+      marginBottom: 15,
+      width: 200,  // Fixed width
+    },
+    title_login: { 
+      fontSize: 15,
+      color: "#000",
+      marginBottom: 15,
+      marginTop: 15, 
+      fontWeight: "bold", 
+    },
+    title_main: { 
+      fontSize: 24,
+      color: "#000",
+      fontWeight: "bold",
+      marginTop: 10,
+    },
+    containerBased: {
+      flex: 1,
+      justifyContent: 'center',
+      padding: 20,
+    },
+    input: {
+      width: 200,  // Fixed width
+      height: 200, 
+      borderColor: 'gray',
+      borderWidth: 1,
+      padding: 10,
+    },
   
+  });
+
+
   export default Home; 
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # BAZAPP
 ## An app for all the bazar you need.
 
-To run this project, use `npm i` and `npm start ios`, `npm start android` or `npm start web.
-
-
+To run this project, use `npm i` or `npm install` to install the dependencies of said project and `npm start ios`, `npm start android` or `npm start web`.
