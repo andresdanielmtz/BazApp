76e736af4376e34d5c01fcf43250ac8168d1acf8
Andrés Martínez <autummata@gmail.com>
2023-10-25 16:54:38 -0700

    fixed image conflict

---
+++ view/Buyer/Events/Events.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import { StyleSheet, Text, View, Image, Button, TextInput } from "react-native";
+import EventsPost from "./EventsPost"; 
+
+const Events = () => {
+    return (
+        <View>
+            <EventsPost /> 
+        </View>
+    )
+}
+
+export default Events; 
